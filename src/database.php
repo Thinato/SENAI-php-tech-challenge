ce881e1db93b6c150cc183277480a02f6c2da8dc
thinato <paulo@brickseguros.com.br>
2023-11-16 22:12:40 -0300

    wip

--- src/database.php
+++ src/database.php
@@ -4,3 +4,7 @@ $url = 'localhost:3306';
 $user = 'root';
 $pass = '1234';
 
+$db = new mysqli($url, $user, $pass);
+
+
+?>
--- view/home.php
+++ view/home.php
@@ -1,7 +1,5 @@
 <?php
 include_once 'static/header.php';
-
-echo 'welcome to home'
 ?>
 
 
